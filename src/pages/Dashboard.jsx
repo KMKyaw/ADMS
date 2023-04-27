41b4549c1d35581867030ed4bd39aeefd76f6fe0
Kyaw <kaungmyatkyaw365@gmail.com>
2023-04-27 17:07:22 +0700

    changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -29,12 +29,44 @@ const options = {
     },
   },
 };
+
+const plugins = [{
+  beforeDraw: function(chart) {
+   var width = chart.width,
+       height = chart.height,
+       ctx = chart.ctx;
+       ctx.restore();
+       var fontSize = (height / 160).toFixed(2);
+       ctx.font = fontSize + "em sans-serif";
+       ctx.textBaseline = "top";
+       var text = "24%",
+       textX = Math.round((width - ctx.measureText(text).width) / 2),
+       textY = height / 2.25;
+       ctx.fillText(text, textX, textY);
+       ctx.save();
+  } 
+}]
+
 export default function Dashboard() {
   return (
     <div>
-      <span className="p-10 text-4xl font-semibold whitespace-nowrap text-navbar">Dashboard</span>
-      <div style={{ width: '50%', height: '400px' }}>
-        <Doughnut className="p-10" data={data} options={options} responsive maintainAspectRatio={false} aspectRatio={1}/>
+      <div className='py-7 px-8'>
+      <span className="md:text-4xl text-2xl font-semibold whitespace-nowrap text-navbar">Dashboard</span>
+      </div>
+      <div className='px-4'>
+        <div className='flex justify-between'>
+          <div className='border-2 relative shadow-md w-[600px]'>
+            <div className="grid grid-cols-2">
+              <div className='w-[100%] md:h-[280px] h-[200px]'>
+                <Doughnut className='p-5' data={data} options={options} plugins={plugins} responsive maintainAspectRatio={false} aspectRatio={1}/>
+              </div>
+              <div className='relative top-[33%]'>
+                <p className="pb-3 md:text-3xl text-xl font-semibold whitespace-nowrap text-navbar">5</p>
+                <p className="md:text-2xl text-xl font-semibold whitespace-nowrap text-navbar">Courses Registered</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

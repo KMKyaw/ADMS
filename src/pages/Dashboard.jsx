1c6bd0d6d3e6ea263f8bf5d83f467b38477b013f
Kyaw <kaungmyatkyaw365@gmail.com>
2023-04-27 23:20:39 +0700

    changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js';
+import { Bar, Doughnut } from 'react-chartjs-2';
+import { black } from 'tailwindcss/colors';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export const data = {
   labels: ['Course'],
@@ -11,8 +12,38 @@ export const data = {
       label: '# of Votes',
       data: [12, 19],
       backgroundColor: [
-        'rgba(168,	210,	109, 0.2)',
-        'rgba(255, 99, 132, 0.2)',
+        'rgba(168,	210,	109, 0.5)',
+        'rgba(252,	239,	233, 1)',
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+
+export const Bardata = {
+  labels: ['2.0', '2.25 ', '2.5', '2.75', '3.0', '3.25', '3.5', '3.75', '4.0'],
+  datasets: [
+    {
+      data: [3, 9, 7, 11, 19, 13, 15, 13, 10],
+      backgroundColor: 'rgba(168,	210,	109, 0.5)',
+      borderColor: 'black',
+      borderWidth: 1,
+    }
+  ]
+}
+export const Studentdata = {
+  labels: ['Studnt'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [45, 14],
+      backgroundColor: [
+        'rgba(168,	210,	109, 0.5)',
+        'rgba(252,	239,	233, 1)',
       ],
       borderColor: [
         'rgba(54, 162, 235, 1)',
@@ -30,6 +61,14 @@ const options = {
   },
 };
 
+const Studentoptions = {
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
 const plugins = [{
   beforeDraw: function(chart) {
    var width = chart.width,
@@ -37,15 +76,35 @@ const plugins = [{
        ctx = chart.ctx;
        ctx.restore();
        var fontSize = (height / 160).toFixed(2);
-       ctx.font = fontSize + "em sans-serif";
+       ctx.font = "bold " + fontSize + "em sans-serif";
        ctx.textBaseline = "top";
        var text = "24%",
        textX = Math.round((width - ctx.measureText(text).width) / 2),
        textY = height / 2.25;
+       ctx.fillStyle = "#373B61";
        ctx.fillText(text, textX, textY);
        ctx.save();
   } 
 }]
+
+const Studentplugins = [{
+  beforeDraw: function(chart) {
+   var width = chart.width,
+       height = chart.height,
+       ctx = chart.ctx;
+       ctx.restore();
+       var fontSize = (height / 160).toFixed(2);
+       ctx.font = "bold " + fontSize + "em sans-serif";
+       ctx.textBaseline = "top";
+       var text = "76%",
+       textX = Math.round((width - ctx.measureText(text).width) / 2),
+       textY = height / 2.25;
+       ctx.fillStyle = "#373B61";
+       ctx.fillText(text, textX, textY);
+       ctx.save();
+  } 
+}]
+
 
 export default function Dashboard() {
   return (
@@ -53,21 +112,62 @@ export default function Dashboard() {
       <div className='py-7 px-8'>
       <span className="md:text-4xl text-2xl font-semibold whitespace-nowrap text-navbar">Dashboard</span>
       </div>
-      <div className='px-4'>
-        <div className='flex justify-between'>
-          <div className='border-2 relative shadow-md w-[600px]'>
-            <div className="grid grid-cols-2">
-              <div className='w-[100%] md:h-[280px] h-[200px]'>
-                <Doughnut className='p-5' data={data} options={options} plugins={plugins} responsive maintainAspectRatio={false} aspectRatio={1}/>
+        <div className='md:grid md:grid-cols-2 grid-cols-1'>
+          <div className='px-2'>
+            <div className='flex justify-between'>
+              <div className='border-2 relative shadow-md w-[100%]'>
+                <div className="grid grid-cols-2">
+                  <div className='w-[100%] md:h-[280px] h-[180px]'>
+                    <Doughnut className='p-5' data={data} options={options} plugins={plugins} responsive maintainAspectRatio={false} aspectRatio={1}/>
+                  </div>
+                  <div className='relative md:top-[33%] top-[25%]'>
+                    <p className="pb-3 md:text-3xl text-xl font-bold whitespace-nowrap text-navbar text-navbar">5</p>
+                    <p className="md:text-2xl text-lg font-semibold whitespace-nowrap text-navbar">Courses Registered</p>
+                  </div>
+                </div>
               </div>
-              <div className='relative top-[33%]'>
-                <p className="pb-3 md:text-3xl text-xl font-semibold whitespace-nowrap text-navbar">5</p>
-                <p className="md:text-2xl text-xl font-semibold whitespace-nowrap text-navbar">Courses Registered</p>
+            </div>
+          </div>
+
+          <div className='px-2 md:pt-0 pt-4'>
+            <div className='flex justify-between'>
+              <div className='border-2 relative shadow-md w-[100%]'>
+                <div className="grid grid-cols-2">
+                  <div className='w-[100%] md:h-[280px] h-[180px]'>
+                    <Doughnut className='p-5' data={Studentdata} options={Studentoptions} plugins={Studentplugins} responsive maintainAspectRatio={false} aspectRatio={1}/>
+                  </div>
+                  <div className='relative md:top-[33%] top-[25%]'>
+                    <p className="pb-3 md:text-3xl text-xl font-bold whitespace-nowrap text-navbar">87</p>
+                    <p className="md:text-2xl text-lg font-semibold whitespace-nowrap text-navbar">Students Registered</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+
+        <div className='p-2'>
+          <div className='border-2 relative shadow-md'>
+            <div className='p-5'>
+              <p className="md:text-2xl text-xl font-bold whitespace-nowrap text-navbar">GPAX Distribution</p>
+            </div>
+            <div className='px-5 md:w-[100%] md:h-[350px] h-[100%] md:pb-2 pb-4'>
+              <Bar
+                data={Bardata} 
+                options={{
+                  plugins: {
+                    legend: {
+                      display: false
+                    }
+                  }
+                }} 
+                responsive 
+                maintainAspectRatio={false} 
+                aspectRatio={1}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
